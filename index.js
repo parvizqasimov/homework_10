faf9e76aeb4ddd9d41aa86c194587ac610562aa7
parvizqasimov <119752037+parvizqasimov@users.noreply.github.com>
2023-04-11 23:48:15 +0400

    frist commit

---
+++ index.js
@@ -0,0 +1,47 @@
+let firstName = "Parviz"
+let lastName = "Qasimov"
+
+function printFullName (lastName,firstName){
+    return (lastName +" " + firstName)};
+    console.log(printFullName(lastName,firstName));
+
+let printFullNameWithParam = (lastName , firstName) => lastName + " " + firstName;
+console.log(printFullName(lastName,firstName));
+
+
+
+let width = 12
+let length = 6
+
+function perimeterOfRectangle (width,length){
+    return (2*(width+length));
+}
+console.log(perimeterOfRectangle(width,length));
+
+
+let weight =70
+let height = 174
+
+let BMI = (weight,height)=>(weight/(((height/100)*(height/100))));
+console.log(BMI(weight,height));
+
+if(BMI<18.5){
+    console.log("Underweight")}
+else if (BMI>=18.5 & BMI<25){
+    console.log("Normal")
+}
+else if (BMI>=25 & BMI<30){
+    console.log("Overweight")
+}
+else {
+    console.log("Obese")
+};
+
+
+
+
+
+
+
+
+
